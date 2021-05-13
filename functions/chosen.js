6706bc037448d80d43644f2bfc2afe155f160cae
craigselbyio <craigselby@att.net>
2021-05-12 20:04:09 -0400

    Initialize git

---
+++ functions/chosen.js
@@ -0,0 +1,29 @@
+let axios = require("axios");
+
+exports.handler = function (context, event, callback) {
+  let body = event.Body.trim();
+
+  axios({
+    url: `https://api.yelp.com/v3/businesses/search?location=${body}&open_now=true&sort_by=rating&categories=restaurants`,
+    headers: { Authorization: `Bearer ${context.YELP_API_KEY}` },
+  })
+    .then((response) => {
+      if (response.status == 200) {
+        // Create variable named businesses from response
+        const { businesses } = response.data;
+
+        // Generate a random index from the businesses array
+        let randomBusinessesIdx = Math.floor(Math.random() * businesses.length);
+
+        // Choose a random Business
+        let chosenBusiness = response.data.businesses[randomBusinessesIdx];
+
+        // Return Business Name & Yelp URL
+        return callback(null, `${chosenBusiness.name} - ${chosenBusiness.url}`);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      return callback(error);
+    });
+};
